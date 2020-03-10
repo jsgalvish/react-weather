610ae111092931666ea394f6d5425b3c73eac597
JS Galvis H <jsgalvish@unal.edu.co>
2020-03-10 11:56:03 -0500

    Updates

---
+++ precache-manifest.ce184528be4c48c397c16c415f39c386.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48f7059746cf0d677c02aa59f8a1abf6",
+    "url": "/react-weather/index.html"
+  },
+  {
+    "revision": "3e3a05304b56352da222",
+    "url": "/react-weather/static/css/2.2f13f71a.chunk.css"
+  },
+  {
+    "revision": "84fc7bd438c20909bb5a",
+    "url": "/react-weather/static/css/main.e3722926.chunk.css"
+  },
+  {
+    "revision": "3e3a05304b56352da222",
+    "url": "/react-weather/static/js/2.3c097907.chunk.js"
+  },
+  {
+    "revision": "84fc7bd438c20909bb5a",
+    "url": "/react-weather/static/js/main.3d00bb48.chunk.js"
+  },
+  {
+    "revision": "e61e452087af7072f6e9",
+    "url": "/react-weather/static/js/runtime~main.2302d1fd.js"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/react-weather/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/react-weather/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/react-weather/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/react-weather/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/react-weather/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
